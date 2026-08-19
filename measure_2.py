17a23215b68a74b6ecd2971b4da5661318522d1b
dougc333 <dougchang25@gmail.com>
2026-08-19 11:53:44 -0700

    add json

--- measure_2.py
+++ measure_2.py
@@ -1,13 +1,13 @@
-import time, torch
+import time, measure_2
 
 t0 = time.perf_counter()
-import torch
+import measure_2
 t1 = time.perf_counter()
 
 from transformers import AutoModelForCausalLM
 t2 = time.perf_counter()
 
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B", dtype=torch.float16)
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B", dtype=measure_2.float16)
 t3 = time.perf_counter()
 
 print(f"import torch:            {t1 - t0:.3f}s")
