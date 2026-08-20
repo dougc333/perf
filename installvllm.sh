f6b44ed2a3a3ed5c0d49c6156760b01db28a3620
dougc333 <dougchang25@gmail.com>
2026-08-19 19:36:44 -0700

    fix installvllm.sh use python3.12 else no work

--- installvllm.sh
+++ installvllm.sh
@@ -2,8 +2,19 @@
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-/root/.local/bin/uv venv venv
-source venv/bin/activate
 
+uv venv --python 3.12 vllm-env
+source vllm-env/bin/activate
 
-/root/.local/bin/uv pip install vllm --torch-backend=auto
+uv self update
+uv pip install -U vllm --torch-backend=cu129
+
+VLLM_USE_FLASHINFER_SAMPLER=0 \
+vllm serve Qwen/Qwen3-0.6B \
+  --model-impl transformers \
+  --enforce-eager
+
+  VLLM_USE_FLASHINFER_SAMPLER=0 \
+vllm serve Qwen/Qwen3-0.6B \
+  --model-impl vllm \
+  --enforce-eager
